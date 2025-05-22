88929d4f07b1a51873277aa38c7c798c4a2942c5
cnagyei <cnagyei15@gmail.com>
2025-05-22 12:51:03 +0000

    LED blink program

--- sketch_may22a/sketch_may22a.ino
+++ sketch_may22a/sketch_may22a.ino
@@ -1,9 +1,22 @@
+/*
+Caleb's first Arduino program
+*/
+
+// Define constants
+int ledPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
-
+  //Initialize pin as output
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(ledPin, HIGH);
+  delay(2000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 
 }
